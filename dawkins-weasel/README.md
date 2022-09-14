700a969b0b2f3ac5defebe9d0dadb40c36223c2d
Erin Young <erinmyoung12@gmail.com>
2022-09-14 16:00:15 +0100

    Add Dawkins' Weasel problem

---
+++ dawkins-weasel/README.md
@@ -0,0 +1,11 @@
+# Dawkins' Weasel Problem
+
+[Programming Praxis - Dawkins' Weasel](https://programmingpraxis.com/2014/11/14/dawkins-weasel/)
+
+## Project set up
+
+In project root:
+`python -m flask run`
+
+Go to:
+http://127.0.0.1:5000/
---
+++ dawkins-weasel/app.py
@@ -0,0 +1,66 @@
+import random
+import string
+from flask import Flask
+
+app = Flask(__name__)
+
+target = 'METHINKS IT IS A WEASEL'
+target_score = len(target)
+
+def get_random():
+    return random.choice(string.ascii_uppercase + ' ')
+
+# 1. Start with a random string of characters the same length as the target
+def get_chars(random_choices):
+    return ''.join(random_choices for _ in range(target_score))
+
+# 2. Make 100 copies of the string (reproduce)
+def reproduce(chars):
+    return [chars] * 100
+
+# 3. For each character in each of the 100 copies, with a probability of 5%, replace (mutate) the character with a new random character.
+def mutate(chars):
+    new_char = ''
+    for c in chars:
+        if random.random() < 0.05:
+            new_char += ''.join(get_random())
+        else:
+            new_char += c
+    return new_char
+
+# 4. Compare each new string with the target string, and give each a score (the number of letters in the string that are correct and in the correct position).
+def score(chars):
+    score = 0
+    for i in range(target_score):
+        if chars[i] == target[i]:
+            score += 1
+    return score
+
+# 5. If any of the new strings has a perfect score (target length), halt. Otherwise, take the highest scoring string, and go to step 2.
+@app.route("/")
+def match_target():
+    """
+    While the score of new random characters doesn't match the target character length:
+
+    1. Create 100 empty strings and None scores
+    2. For each sequence in 100, assign empty copies at that sequence to mutated chars and empty scores to the score of mutated chars
+    3. Check the highest score (target char length) and append the chars at this best score's index to the empty array
+    4. Increment loop index to see how many iterations it took to match the target
+    """
+    mutated_target = []
+    loop_index = 0
+    new_chars = get_chars(get_random())
+    while score(new_chars) != target_score:
+        strings = reproduce('')
+        scores = reproduce(None)
+        for i in range(100):
+            strings[i] = mutate(new_chars)
+            scores[i] = score(strings[i])
+        high_score = max(scores)
+        new_chars = strings[scores.index(high_score)]
+        mutated_target.append(new_chars)
+        loop_index += 1
+    return '\n'.join(mutated_target)
+
+if __name__ == '__main__':
+  app.run(debug=True)
