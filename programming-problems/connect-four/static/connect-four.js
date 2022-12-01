a4cbb3faf986adf9568b0221d8240bb38835bf5a
Erin Young <erinmyoung12@gmail.com>
2022-12-01 08:46:00 +0000

    Add styling, second player, and update js

--- programming-problems/connect-four/static/connect-four.js
+++ programming-problems/connect-four/static/connect-four.js
@@ -6,6 +6,10 @@ class ConnectFour {
     this.board = this.table.querySelector('.board');
     this.rows = this.board.querySelectorAll('tr[data-index]');
 
+    // Players
+    this.playerOne = document.getElementById('player_1');
+    this.playerTwo = document.getElementById('player_2');
+
     // Canvas
     this.canvasOne = document.getElementById('game_canvas_one');
     this.canvasTwo = document.getElementById('game_canvas_two');
@@ -15,11 +19,25 @@ class ConnectFour {
   }
 
   run() {
+    this.setComputerColor();
     this.playerPiece(this.canvasOne);
+    this.playerPiece(this.canvasTwo);
+    this.currentPlayer(this.playerOne);
     this.emptyCanvas.forEach((canvas) => this.playerPiece(canvas));
     this.buttons.forEach((button) => {
       button.addEventListener('click', this.movePiece.bind(this));
     });
+  }
+
+  setComputerColor() {
+    const playerOne = document.querySelector('.player_details[data-color]');
+    if (playerOne.dataset.color === '#cc0000') {
+      this.canvasTwo.style.color = '#ffc34d';
+      document.querySelector('.computer_color').innerHTML = 'Color: #ffc34d';
+    } else {
+      this.canvasTwo.style.color = '#cc0000';
+      document.querySelector('.computer_color').innerHTML = 'Color: #cc0000';
+    }
   }
 
   movePiece(e) {
@@ -44,11 +62,19 @@ class ConnectFour {
     }
   }
 
+  currentPlayer(player) {
+    const message = document.createElement('span');
+    const child = player.querySelector('.name');
+    message.innerHTML = 'Current player';
+    message.style.color = 'green';
+    player.insertBefore(message, child);
+  }
+
   playerPiece(canvas) {
     const ctx = canvas.getContext('2d');
     ctx.beginPath();
     ctx.arc(25, 25, 25, 0, 2 * Math.PI);
     ctx.fillStyle = canvas.style.color;
     ctx.fill();
-  }  
+  }
 }
